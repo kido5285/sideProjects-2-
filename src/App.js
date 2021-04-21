9aa888c29e8e62226f003d37ad171cb8a5a88e97
kido5285 <75468540+kido5285@users.noreply.github.com>
2021-04-21 16:35:20 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Topsection from './reactcomponent/Topsection';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App"> 
+      <Topsection/>
+    </div>
+  );
+}
+
+export default App;
